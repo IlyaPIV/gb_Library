c40d43e804a42fc7f139f44e8955c5446153cc04
kolobkevic <kolobkevic@gmail.com>
2023-06-13 21:46:05 +0300

    Security with JWT token. Все заработало

--- LibraryWebParent/LibraryAuthService/src/main/java/gb/library/auth/configs/SecurityConfig.java
+++ LibraryWebParent/LibraryAuthService/src/main/java/gb/library/auth/configs/SecurityConfig.java
@@ -2,14 +2,12 @@ package gb.library.auth.configs;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -25,13 +23,10 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/authenticate").permitAll()
+                .requestMatchers("/authenticate/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 .and()
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authenticationProvider(authenticationProvider);
--- LibraryWebParent/LibraryAuthService/src/main/java/gb/library/auth/controllers/AuthController.java
+++ LibraryWebParent/LibraryAuthService/src/main/java/gb/library/auth/controllers/AuthController.java
@@ -6,14 +6,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/authenticate")
 public class AuthController {
     private final AuthenticationService authService;
 
-    @PostMapping("/authenticate")
+    @PostMapping
     public ResponseEntity<?> authenticate(@RequestBody JwtRequest request) {
         return authService.authenticate(request);
     }
