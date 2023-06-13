dfda1603a491c64663b2aa0625d6a8be3d0099c5
kolobkevic <kolobkevic@gmail.com>
2023-06-13 22:42:32 +0300

    Security with JWT token. Все заработало

--- LibraryWebParent/LibraryAuthService/src/main/java/gb/library/auth/services/AuthenticationService.java
+++ LibraryWebParent/LibraryAuthService/src/main/java/gb/library/auth/services/AuthenticationService.java
@@ -2,12 +2,9 @@ package gb.library.auth.services;
 
 import gb.library.auth.dtos.JwtRequest;
 import gb.library.auth.dtos.JwtResponse;
-import gb.library.common.exceptions.AppError;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
